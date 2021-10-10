beeb24bc0a337ae439388590ef5e67f022d7e799
jayeshk-star <jayeshkale052@gmail.com>
2021-10-10 18:06:13 +0530

    header part done with icons

--- myapp/src/compoents/Conatin.jsx
+++ myapp/src/compoents/Conatin.jsx
@@ -4,8 +4,15 @@ import Card from './Card'
 const Conatin = () => {
   return (
     <div>
-      <h1 style={{ color: 'white' }}>HELIGHTS</h1>
-
+      <h1
+        style={{
+          color: 'white',
+          marginLeft: '20px',
+          textDecoration: 'underline'
+        }}
+      >
+        Highlight{' '}
+      </h1>
       <Card />
     </div>
   )
--- myapp/src/compoents/Header.jsx
+++ myapp/src/compoents/Header.jsx
@@ -1,30 +1,54 @@
 import React from 'react'
 import './Header.css'
-// import { AiFillStar } from 'react-icons/ai';
-var icons = [
-  'Featured',
-  'Armoury',
-  'Aquatics',
-  'Cycling',
-  'Equestrian',
-  'Extreme',
-  'Golf',
-  'Gymanastics',
-  'Lake&Sea'
-]
+import { AiFillStar } from 'react-icons/ai'
+import { GiSwissArmyKnife } from 'react-icons/gi'
+import { GiStrongMan } from 'react-icons/gi'
+import { GiCycling } from 'react-icons/gi'
+import { GiSeahorse } from 'react-icons/gi'
+import { MdAirlineSeatReclineExtra } from 'react-icons/md'
+import { GiBaseballBat } from 'react-icons/gi'
+import { SiOpenaigym } from 'react-icons/si'
+import { GiSeaCreature } from 'react-icons/gi'
 
 const Header = () => {
   return (
     <div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
-      <div className='icon_contain'></div>
+      <div className='icon_contain'>
+        <AiFillStar className='logostyle' />
+        <div className='logoname'>Featured</div>
+      </div>
+      <div className='icon_contain'>
+        <GiSwissArmyKnife className='logostyle' />
+        <div className='logoname'>Armoury</div>
+      </div>
+      <div className='icon_contain'>
+        <GiStrongMan className='logostyle' />
+        <div className='logoname'>Aquatics</div>
+      </div>
+      <div className='icon_contain'>
+        <GiCycling className='logostyle' />
+        <div className='logoname'>Cycling</div>
+      </div>
+      <div className='icon_contain'>
+        <GiSeahorse className='logostyle' />
+        <div className='logoname'>Equestrian</div>
+      </div>
+      <div className='icon_contain'>
+        <MdAirlineSeatReclineExtra className='logostyle' />
+        <div className='logoname'>Extreme</div>
+      </div>
+      <div className='icon_contain'>
+        <GiBaseballBat className='logostyle' />
+        <div className='logoname'>Golf</div>
+      </div>
+      <div className='icon_contain'>
+        <SiOpenaigym className='logostyle' />
+        <div className='logoname'>Gymanastics</div>
+      </div>
+      <div className='icon_contain'>
+        <GiSeaCreature className='logostyle' />
+        <div className='logoname'>Lake&Sea</div>
+      </div>
     </div>
   )
 }
